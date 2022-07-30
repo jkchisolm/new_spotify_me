5ec512b178bd0519ad0054874b979285291b46e1
Joshua Chisolm <jkchisolm22@gmail.com>
2022-07-29 21:00:56 -0700

    Add some nice background transitioning

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../components/index/Banner/Banner';
 import Favorites from '../components/mepage/favorites/Favorites';
 import { useSpotifyStore } from '../lib/store/zustand';
+import styles from '../styles/Me.module.css';
 
 const Me = () => {
   const [currentOption, setCurrentOption] = useState(1);
+
+  const [bgSetting, setBgSetting] = useState(styles['bg-step-one']);
 
   const spotifyState = useSpotifyStore();
 
@@ -12,8 +15,38 @@ const Me = () => {
     setCurrentOption(option);
   };
 
+  const listenScrollEvent = (event: any) => {
+    console.log(window.scrollY);
+    if (window.scrollY < 150) {
+      setBgSetting(styles['bg-step-one']);
+    } else if (window.scrollY < 300) {
+      setBgSetting(styles['bg-step-two']);
+    } else if (window.scrollY < 450) {
+      setBgSetting(styles['bg-step-three']);
+    }
+    // switch (window.scrollY) {
+    //   case 0:
+    //     setBgSetting(styles['bg-step-one']);
+    //     break;
+    //   case 150:
+    //     setBgSetting(styles['bg-step-two']);
+    //     break;
+    //   case 300:
+    //     setBgSetting(styles['bg-step-three']);
+    //     break;
+    //   default:
+    //     break;
+    // }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+  });
+
   return (
-    <div className="h-full min-h-screen w-full flex flex-col justify-center items-center bg-gradient-to-br from-lime-300 to-cyan-400">
+    <div
+      className={`h-full min-h-screen w-full flex flex-col justify-center items-center ${bgSetting}`}
+    >
       <div className="mb-32">
         <Banner />
       </div>
